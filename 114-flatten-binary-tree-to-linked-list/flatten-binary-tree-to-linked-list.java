de0740befe17ce05ac011f2f67541e8c6f9b646d
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-26 11:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (85.93%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -30,3 +30,18 @@ class Solution {
     }
 }
 
+
+
+//1. REVERSE PREORDER [DFS]
+
+// class Solution {
+//         TreeNode prev=null;
+//     public void flatten(TreeNode root) {
+//         if(root==null)return;
+//         flatten(root.right);
+//         flatten(root.left);
+//         root.right=prev;
+//         root.left=null;
+//         prev=root;
+//     }
+// }
